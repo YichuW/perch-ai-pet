6f185631b3bc7b85719652220046af318428aed9
yhg01 <gaoyuhe66@gmail.com>
2026-04-14 19:42:45 -0400

    add demo trigger to preload for testing proactive reminders

--- electron/preload.js
+++ electron/preload.js
@@ -9,4 +9,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   onProfileLoaded: (cb) => ipcRenderer.on('profile-loaded', (_e, data) => cb(data)),
   onSettingsLoaded: (cb) => ipcRenderer.on('settings-loaded', (_e, data) => cb(data)),
   onPetUpdate: (cb) => ipcRenderer.on('pet-update', (_e, data) => cb(data)),
+
+  // Demo: fire a simulated 60-min work reminder
+  demoTrigger: () => ipcRenderer.invoke('demo-trigger'),
 })
