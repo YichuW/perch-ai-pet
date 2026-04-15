011483b36fd2609d2e30918a73f3cd717d73bc29
chloeee <chloeee@chloeeedeMacBook-Pro.local>
2026-04-14 23:51:53 -0400

    wip: update main.css

--- src/renderer/components/WelcomeScreen.tsx
+++ src/renderer/components/WelcomeScreen.tsx
@@ -1,18 +1,13 @@
 import { useAppStore } from '../store/useAppStore';
+import happyCat from '../../assets/pet/cat_idle.png';
 
 export default function WelcomeScreen() {
   const setScreen = useAppStore((s) => s.setScreen);
 
   return (
     <div className="screen welcome-screen">
-      <div className="cat-face">
-        <div className="cat-ear left-ear" />
-        <div className="cat-ear right-ear" />
-        <div className="cat-eyes">
-          <span />
-          <span />
-        </div>
-        <div className="cat-mouth">﹀</div>
+      <div className="welcome-pet">
+        <img src={happyCat} alt="pet" className="welcome-pet-image" />
       </div>
 
       <h1 className="title">Hi! I&apos;m your new desktop companion.</h1>
